06572b15fab6f168213f2bb502691dc9b9f6cb3a
Stephen Seo <seo.disparate@gmail.com>
2014-05-02 11:31:59 -0400

    Setup for undo feature

---
+++ src/command.cpp
@@ -0,0 +1,58 @@
+
+#include "command.hpp"
+
+CommandStack::CommandStack() :
+executed(),
+undone()
+{}
+
+CommandStack::~CommandStack()
+{
+    clearExecuted();
+    clearUndone();
+}
+
+void CommandStack::execute(Command* c)
+{
+    c->execute();
+    executed.push_front(c);
+    clearUndone();
+}
+
+bool CommandStack::undo()
+{
+    if(executed.size() == 0)
+        return false;
+    Command* c = executed.front();
+    executed.pop_front();
+    c->undo();
+    undone.push_front(c);
+    return true;
+}
+
+bool CommandStack::redo()
+{
+    if(undone.size() == 0)
+        return false;
+    Command* c = undone.front();
+    undone.pop_front();
+    c->execute();
+    executed.push_front(c);
+    return true;
+}
+
+void CommandStack::clearExecuted()
+{
+    for(auto iter = executed.begin(); iter != executed.end(); ++iter)
+        delete *iter;
+
+    executed.clear();
+}
+
+void CommandStack::clearUndone()
+{
+    for(auto iter = undone.begin(); iter != undone.end(); ++iter)
+        delete *iter;
+
+    undone.clear();
+}
